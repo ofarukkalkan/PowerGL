0a650b481419fe581025a14739bfc884ca53b198
Omer Faruk Kalkan <omerfarukkalkan@outlook.com.tr>
2018-09-04 02:12:12 +0300

    window interface struct added

--- src/util/window/window.h
+++ src/util/window/window.h
@@ -1,5 +1,15 @@
 #ifndef _powergl_util_window_h
 #define _powergl_util_window_h
 
+typedef struct powergl_util_window_t powergl_util_window;
+typedef int powergl_util_window_create_func_t (void);
+ 
+struct powergl_util_window_t {
+  
+  powergl_util_window_create_func_t* create;
+ 
+};
+
+powergl_util_window* powergl_util_window_create(void);
 
 #endif
