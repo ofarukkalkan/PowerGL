d269375a54244f1c6628c22bbf5609c5908d4157
Omer Faruk Kalkan <omerfarukkalkan@outlook.com.tr>
2018-09-05 00:51:58 +0300

    powergl_util_window added X11 window creation support

--- src/util/window/window.c
+++ src/util/window/window.c
@@ -1,7 +1,8 @@
 #include "window.h"
+#include "xorg/xorg.h"
 
 powergl_util_window* powergl_util_window_create(void){
   powergl_util_window* w;
-
+  w = (powergl_util_window*) powergl_util_window_xorg_create();
   return w;
 }
--- src/util/window/window.h
+++ src/util/window/window.h
@@ -1,6 +1,8 @@
 #ifndef _powergl_util_window_h
 #define _powergl_util_window_h
 
+#include "../../powergl.h"
+
 typedef struct powergl_util_window_t powergl_util_window;
 typedef int powergl_util_window_create_func_t (void);
  
