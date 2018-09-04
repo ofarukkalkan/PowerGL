cdc99f455aeebd08f739a098d8a8f1810e3fb898
Omer Faruk Kalkan <omerfarukkalkan@outlook.com.tr>
2018-09-04 14:56:46 +0300

    powergl_resize fonksiyonu eklendi

--- src/powergl.c
+++ src/powergl.c
@@ -1,1 +1,13 @@
 #include "powergl.h"
+
+void* powergl_resize(void *ptr,size_t newsize){
+  if(ptr==NULL){
+    ptr = malloc(newsize);
+    assert(ptr);
+  }else{
+    void* tmp_ptr = realloc(ptr,newsize);
+    assert(tmp_ptr);
+    ptr = tmp_ptr;
+  }
+  return ptr;
+}
--- src/powergl.h
+++ src/powergl.h
@@ -1,5 +1,10 @@
 #ifndef _powergl_h
 #define _powergl_h
 
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+void* powergl_resize(void* ptr,size_t newsize);
 
 #endif
