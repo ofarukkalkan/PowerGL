fb7ebf5bebf3757207fcf098a74994fa62972448
Omer Faruk Kalkan <omerfarukkalkan@outlook.com.tr>
2018-09-21 01:22:41 +0300

    matrix 4x4 inverse, lookat and perspective projection added

--- src/math/mat4x4.c
+++ src/math/mat4x4.c
@@ -1,5 +1,9 @@
 #include "mat4x4.h"
+
+#include <float.h>
 #include <stdio.h>
+#include <assert.h>
+
 void powergl_print4x4(float m[4][4]){
   unsigned i,j;
   printf("\n");
@@ -11,6 +15,7 @@ void powergl_print4x4(float m[4][4]){
     printf(" ]\n");
   }
 }
+
 void powergl_zero4x4(GLfloat m[4][4]){
   m[0][0]=0.0f;
   m[0][1]=0.0f;
@@ -29,6 +34,7 @@ void powergl_zero4x4(GLfloat m[4][4]){
   m[3][2]=0.0f;
   m[3][3]=0.0f;
 }
+
 void powergl_ident4x4(GLfloat m[4][4]){
   m[0][0]=1.0f;
   m[0][1]=0.0f;
@@ -47,6 +53,7 @@ void powergl_ident4x4(GLfloat m[4][4]){
   m[3][2]=0.0f;
   m[3][3]=1.0f;
 }
+
 void powergl_mult4x4(GLfloat m[4][4],GLfloat n[4][4],GLfloat r[4][4]){
   r[0][0]=m[0][0]*n[0][0] +m[0][1]*n[1][0] + m[0][2]*n[2][0] + m[0][3]*n[3][0];
   r[0][1]=m[0][0]*n[0][1] +m[0][1]*n[1][1] + m[0][2]*n[2][1] + m[0][3]*n[3][1];
@@ -89,16 +96,59 @@ void powergl_mult4x4(GLfloat m[4][4],GLfloat n[4][4],GLfloat r[4][4]){
   m[3][3]=r[3][3];
   
 }
+
 void powergl_trans4x4(GLfloat m[4][4]){
-  m[0][0] = m[0][0];
-  m[0][1] = m[1][0];
-  m[0][2] = m[2][0];
-  m[0][3] = m[3][0];
-  m[1][0] = m[0][1];
-  m[1][1] = m[1][1];
-  m[1][2] = m[2][1];
-  m[1][3] = m[3][1];
+	GLfloat n[4][4];
+	
+	for(size_t i=0; i<4; ++i){
+		for(size_t j=0; j<4; ++j){
+			n[j][i] = m[i][j];
+		}
+	}
+	
+	for(size_t i=0; i<4; ++i){
+		for(size_t j=0; j<4; ++j){
+			m[i][j] = n[i][j];
+		}
+	}
 }
+
+int powergl_inv4x4(GLfloat m[16],GLfloat invOut[16] ){
+	GLfloat inv[16];
+	GLfloat det = 0.0f;
+ 
+	inv[ 0] =  m[5] * m[10] * m[15] - m[5] * m[11] * m[14] - m[9] * m[6] * m[15] + m[9] * m[7] * m[14] + m[13] * m[6] * m[11] - m[13] * m[7] * m[10];
+	inv[ 4] = -m[4] * m[10] * m[15] + m[4] * m[11] * m[14] + m[8] * m[6] * m[15] - m[8] * m[7] * m[14] - m[12] * m[6] * m[11] + m[12] * m[7] * m[10];
+	inv[ 8] =  m[4] * m[ 9] * m[15] - m[4] * m[11] * m[13] - m[8] * m[5] * m[15] + m[8] * m[7] * m[13] + m[12] * m[5] * m[11] - m[12] * m[7] * m[ 9];
+	inv[12] = -m[4] * m[ 9] * m[14] + m[4] * m[10] * m[13] + m[8] * m[5] * m[14] - m[8] * m[6] * m[13] - m[12] * m[5] * m[10] + m[12] * m[6] * m[ 9];
+	inv[ 1] = -m[1] * m[10] * m[15] + m[1] * m[11] * m[14] + m[9] * m[2] * m[15] - m[9] * m[3] * m[14] - m[13] * m[2] * m[11] + m[13] * m[3] * m[10];
+	inv[ 5] =  m[0] * m[10] * m[15] - m[0] * m[11] * m[14] - m[8] * m[2] * m[15] + m[8] * m[3] * m[14] + m[12] * m[2] * m[11] - m[12] * m[3] * m[10];
+	inv[ 9] = -m[0] * m[ 9] * m[15] + m[0] * m[11] * m[13] + m[8] * m[1] * m[15] - m[8] * m[3] * m[13] - m[12] * m[1] * m[11] + m[12] * m[3] * m[ 9];
+	inv[13] =  m[0] * m[ 9] * m[14] - m[0] * m[10] * m[13] - m[8] * m[1] * m[14] + m[8] * m[2] * m[13] + m[12] * m[1] * m[10] - m[12] * m[2] * m[ 9];
+	inv[ 2] =  m[1] * m[ 6] * m[15] - m[1] * m[ 7] * m[14] - m[5] * m[2] * m[15] + m[5] * m[3] * m[14] + m[13] * m[2] * m[ 7] - m[13] * m[3] * m[ 6];
+	inv[ 6] = -m[0] * m[ 6] * m[15] + m[0] * m[ 7] * m[14] + m[4] * m[2] * m[15] - m[4] * m[3] * m[14] - m[12] * m[2] * m[ 7] + m[12] * m[3] * m[ 6];
+	inv[10] =  m[0] * m[ 5] * m[15] - m[0] * m[ 7] * m[13] - m[4] * m[1] * m[15] + m[4] * m[3] * m[13] + m[12] * m[1] * m[ 7] - m[12] * m[3] * m[ 5];
+	inv[14] = -m[0] * m[ 5] * m[14] + m[0] * m[ 6] * m[13] + m[4] * m[1] * m[14] - m[4] * m[2] * m[13] - m[12] * m[1] * m[ 6] + m[12] * m[2] * m[ 5];
+	inv[ 3] = -m[1] * m[ 6] * m[11] + m[1] * m[ 7] * m[10] + m[5] * m[2] * m[11] - m[5] * m[3] * m[10] - m[ 9] * m[2] * m[ 7] + m[ 9] * m[3] * m[ 6];
+	inv[ 7] =  m[0] * m[ 6] * m[11] - m[0] * m[ 7] * m[10] - m[4] * m[2] * m[11] + m[4] * m[3] * m[10] + m[ 8] * m[2] * m[ 7] - m[ 8] * m[3] * m[ 6];
+	inv[11] = -m[0] * m[ 5] * m[11] + m[0] * m[ 7] * m[ 9] + m[4] * m[1] * m[11] - m[4] * m[3] * m[ 9] - m[ 8] * m[1] * m[ 7] + m[ 8] * m[3] * m[ 5];
+	inv[15] =  m[0] * m[ 5] * m[10] - m[0] * m[ 6] * m[ 9] - m[4] * m[1] * m[10] + m[4] * m[2] * m[ 9] + m[ 8] * m[1] * m[ 6] - m[ 8] * m[2] * m[ 5];
+ 
+	det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+ 
+	if(det == 0.0f){
+		return 0;
+	}
+		
+	det = 1.0f / det;
+ 
+	for(size_t i = 0; i < 16; i++){
+		invOut[i] = inv[i] * det;
+	}
+ 
+	return 1;
+}
+
 void powergl_rot4x4(GLfloat m[4][4],GLfloat n[4][4],float angle,powergl_vec3* axis){
   float a,c,s,tmp,rot[9];
   powergl_vec3 temp;
@@ -147,3 +197,52 @@ void powergl_rot4x4(GLfloat m[4][4],GLfloat n[4][4],float angle,powergl_vec3* ax
   m[2][1]=n[2][1];
   m[2][2]=n[2][2];
  }
+
+
+/* creating rigth handed and 0 to 1 depth ranged perspective projection transformation */
+void powergl_perspectiveRH(GLfloat projection[4][4],float fovy,float aspect,float zNear,float zFar){
+  assert(fabs(aspect - FLT_EPSILON) > 0.0f);
+  const float tanHalfFovy = tanf(fovy / 2.0f);
+  powergl_zero4x4(projection);
+  projection[0][0] = 1.0f / (aspect * tanHalfFovy);
+  projection[1][1] = 1.0f / (tanHalfFovy);
+  projection[2][3] = -1.0f;
+  /* depth range 0 to 1
+  projection[2][2] = zFar / (zFar - zNear);
+  projection[3][2] = -(zFar * zNear) / (zFar - zNear);
+  */
+  /* depth range -1 to 1*/
+  projection[2][2] = - (zFar + zNear) / (zFar - zNear);
+  projection[3][2] = - (2.0f * zFar * zNear) / (zFar - zNear);
+  
+}
+
+/* creating view transformation */
+void powergl_lookat(GLfloat view[4][4],powergl_vec3* eye,powergl_vec3* target,powergl_vec3* up){
+  powergl_vec3 f;
+  powergl_vec3 s;
+  powergl_vec3 u;
+
+  powergl_subvec3(target,eye,&f);
+  powergl_normvec3(&f,&f);
+
+  powergl_crossvec3(&f,up,&s);
+  powergl_normvec3(&s,&s);
+
+  powergl_crossvec3(&s,&f,&u);
+
+  powergl_ident4x4(view);
+  
+  view[0][0] = s.x;
+  view[1][0] = s.y;
+  view[2][0] = s.z;
+  view[0][1] = u.x;
+  view[1][1] = u.y;
+  view[2][1] = u.z;
+  view[0][2] =-f.x;
+  view[1][2] =-f.y;
+  view[2][2] =-f.z;
+  view[3][0] =-powergl_dotvec3(&s, eye);
+  view[3][1] =-powergl_dotvec3(&u, eye);
+  view[3][2] = powergl_dotvec3(&f, eye);
+}
