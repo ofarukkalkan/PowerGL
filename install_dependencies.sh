fa978fb592776f686b14b364aaf95fabac75100d
Ömer Faruk Kalkan <ofarukkalkan@users.noreply.github.com>
2025-06-13 19:47:18 +0300

    Add installation script for Autotools dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+PACKAGES=(
+  autoconf
+  automake
+  libtool
+  pkg-config
+  build-essential
+  libgl1-mesa-dev
+  libx11-dev
+  libexpat1-dev
+  libsdl2-dev
+  libglew-dev
+  libpng-dev
+)
+
+if [ "$EUID" -ne 0 ]; then
+  sudo apt-get update
+  sudo apt-get install -y "${PACKAGES[@]}"
+else
+  apt-get update
+  apt-get install -y "${PACKAGES[@]}"
+fi
+
