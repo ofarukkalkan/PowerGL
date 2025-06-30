0999ed10c257faaa1059d8ddf7b51605a2a301f6
Ömer Faruk Kalkan <ofarukkalkan@users.noreply.github.com>
2025-07-01 00:47:38 +0300

    Expose matrix decomposition API

--- src/math/mat4x4.c
+++ src/math/mat4x4.c
@@ -483,3 +483,35 @@ inline powergl_vec3 powergl_unproject( powergl_vec3 win, powergl_mat4 mvp, power
   return result.xyz;
   // printf("w division pos = %f %f %f\n", result->x, result->y, result->z);
 }
+
+inline void powergl_decompose_matrix(powergl_mat4 m,
+                                     powergl_vec3 *loc,
+                                     powergl_vec3 *rot,
+                                     powergl_vec3 *sca) {
+  if(loc){
+    loc->x = m.c[3].x;
+    loc->y = m.c[3].y;
+    loc->z = m.c[3].z;
+  }
+
+  if(sca){
+    sca->x = sqrtf(m.c[0].x * m.c[0].x + m.c[0].y * m.c[0].y + m.c[0].z * m.c[0].z);
+    sca->y = sqrtf(m.c[1].x * m.c[1].x + m.c[1].y * m.c[1].y + m.c[1].z * m.c[1].z);
+    sca->z = sqrtf(m.c[2].x * m.c[2].x + m.c[2].y * m.c[2].y + m.c[2].z * m.c[2].z);
+  }
+
+  powergl_mat4 r = m;
+  r.c[0].x /= sca->x; r.c[0].y /= sca->x; r.c[0].z /= sca->x;
+  r.c[1].x /= sca->y; r.c[1].y /= sca->y; r.c[1].z /= sca->y;
+  r.c[2].x /= sca->z; r.c[2].y /= sca->z; r.c[2].z /= sca->z;
+
+  float ry = asinf(r.c[2].x);
+  float rx = atan2f(-r.c[2].y, r.c[2].z);
+  float rz = atan2f(-r.c[1].x, r.c[0].x);
+
+  if(rot){
+    rot->x = rx;
+    rot->y = ry;
+    rot->z = rz;
+  }
+}
