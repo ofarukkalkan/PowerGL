54fb2fb1c9316d2b9f1eb7110661667a7b41891c
Ömer Faruk Kalkan <ofarukkalkan@users.noreply.github.com>
2025-06-30 07:29:48 +0300

    Fix distcheck test path

--- tests/run_vertexcoloredcube_headless.sh
+++ tests/run_vertexcoloredcube_headless.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-POWERGL_HEADLESS=1 EGL_PLATFORM=surfaceless "$(dirname "$0")"/vertexcoloredcube_demo
+# Automake executes tests from their build directory, so the built
+# vertexcoloredcube_demo binary is available in the current working
+# directory.  Simply invoke it directly to work both in the build tree
+# and in "make distcheck".
+POWERGL_HEADLESS=1 EGL_PLATFORM=surfaceless ./vertexcoloredcube_demo
