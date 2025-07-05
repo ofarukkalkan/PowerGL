95e70102bb96f13eaaa3881f4a599b6c2006c79a
Ömer Faruk Kalkan <ofarukkalkan@users.noreply.github.com>
2025-07-05 19:15:14 +0300

    Update orientation test

--- tests/gtest_collada.cpp
+++ tests/gtest_collada.cpp
@@ -53,8 +53,8 @@ TEST(ObjectImport, AxisConversion){
     powergl_mat4 w = cube->transform.world;
     EXPECT_NEAR(w.c[0].x, 1.0f, 1e-5f);
     EXPECT_NEAR(w.c[1].y, 0.0f, 1e-5f);
-    EXPECT_NEAR(w.c[1].z, -1.0f, 1e-5f);
-    EXPECT_NEAR(w.c[2].y, 1.0f, 1e-5f);
+    EXPECT_NEAR(w.c[1].z, 1.0f, 1e-5f);
+    EXPECT_NEAR(w.c[2].y, -1.0f, 1e-5f);
     EXPECT_NEAR(w.c[2].z, 0.0f, 1e-5f);
 }
 
